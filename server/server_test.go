036e2cc158ba76fe36a5c5e1bed3a0328f8c704f
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-02 20:03:12 -0300

    add basic routing

--- server/server_test.go
+++ server/server_test.go
@@ -19,5 +19,19 @@ func TestGETPlayers(t *testing.T) {
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
-	})	
+	})
+	
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
