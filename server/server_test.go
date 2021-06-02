300ede5514985af271d398c8c6741f8a28ff1132
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-02 19:55:03 -0300

    refactor test to Run function

--- server/server_test.go
+++ server/server_test.go
@@ -7,15 +7,17 @@ import (
 )
 
 func TestGETPlayers(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
-	response := httptest.NewRecorder()
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
 
-	PlayerServer(response, request)
+		PlayerServer(response, request)
 
-	got := response.Body.String()
-	want := "20"
+		got := response.Body.String()
+		want := "20"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})	
 }
