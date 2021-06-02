b7b1a025a6f430b7abc5d3d434ee86401b327f04
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-02 20:39:06 -0300

    refactor to use interfaces

--- server/main.go
+++ server/main.go
@@ -5,7 +5,15 @@ import (
 	"net/http"
 )
 
+// Run the Application -> InMemoryPlayerStore
+
+type InMemoryPlayerStore struct {}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	log.Fatal(http.ListenAndServe(":5000", handler))
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
